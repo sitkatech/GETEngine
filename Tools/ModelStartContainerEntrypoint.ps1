b22ad5003b1681bc9b746c050407392b60cb78d7
stewartgordonsitka <stewart.gordon@sitkatech.com>
2023-07-31 07:19:34 -0700

    * extract agent zip in startup for containers

--- Tools/ModelStartContainerEntrypoint.ps1
+++ Tools/ModelStartContainerEntrypoint.ps1
@@ -1,8 +1,11 @@
 Write-Host Start container
 
 Write-Host Download latest agent build
-$agentPath = 'https://' + $env:STORAGE_ACCOUNT + '.file.core.windows.net/agent/' + $env:SAS_TOKEN + '';
+$agentPath = 'https://' + $env:STORAGE_ACCOUNT + '.file.core.windows.net/agent/?' + $env:SAS_TOKEN + '';
 & 'C:\\azcopy\\azcopy.exe' copy $agentPath 'C:\\' --recursive
+
+Write-Host Extract latest agent build
+& Expand-Archive -LiteralPath 'C:\\agent\\Olsson.GET.Clients.Agent.zip' -DestinationPath 'C:\\agent\\'
 
 Write-Host Run Agent
 & C:\\agent\\Olsson.GET.Clients.Agent.exe $env:RUN_ID $env:PROCESSTYPE
